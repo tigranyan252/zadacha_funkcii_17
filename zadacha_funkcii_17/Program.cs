7cf5893ef4df9ef5c67d1e0e49c45c2c1d3a976e
Serj Tigranyan <serj-tigranyan@mail.ru>
2021-12-23 23:32:33 +0200

    Добавьте файлы проекта.

---
+++ zadacha_funkcii_17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zadacha_5_17_variant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter base");
+            int Base = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number");
+            char[] Num = Console.ReadLine().ToCharArray();
+            int sum = 0;
+
+            for (int i = Num.Length - 1; i >= 0; i--)
+            {
+                int digit = int.Parse(Num[i].ToString());
+                sum += digit * (int)Math.Pow(Base, Math.Abs(i - (Num.Length - 1)));
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
